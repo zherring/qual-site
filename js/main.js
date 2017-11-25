d3a64b3ad539c9054b6262995ac7fd1e39d9db38
zherring <z.herring@gmail.com>
2017-11-25 16:57:50 -0700

    done for now

--- js/main.js
+++ js/main.js
@@ -85,3 +85,20 @@ progress.addEventListener('click', (e) => scrub(e));
 // progress.addEventListener('mouseup', () => mousedown = false);
 
 audio.addEventListener('timeupdate', updateTime, false);
+
+
+/// Smooth Scroll
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+      }, 500);
+        return false;
+      }
+    }
+  });
+});
